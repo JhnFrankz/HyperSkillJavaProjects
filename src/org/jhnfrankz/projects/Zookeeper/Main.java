a32d234a25424de8d010e629ca60cc464257c1ea
Jhan Frank <jhnfrankz@gmail.com>
2024-03-19 18:51:45 -0500

    Stage 1/4:Print first lines of code

--- src/org/jhnfrankz/projects/Zookeeper/Main.java
+++ src/org/jhnfrankz/projects/Zookeeper/Main.java
@@ -1,4 +1,12 @@
 package org.jhnfrankz.projects.Zookeeper;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                I love animals!
+                Let's check on the animals...
+                The deer looks fine.
+                The bat looks happy.
+                The lion looks healthy.""");
+    }
 }
