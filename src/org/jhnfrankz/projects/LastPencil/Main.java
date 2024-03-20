393af57f9ca4b8273343d622e606473754e6c18c
Jhan Frank <jhnfrankz@gmail.com>
2024-03-20 09:16:56 -0500

    Stage 1/5:Empty the pencil case

--- src/org/jhnfrankz/projects/LastPencil/Main.java
+++ src/org/jhnfrankz/projects/LastPencil/Main.java
@@ -2,6 +2,8 @@ package org.jhnfrankz.projects.LastPencil;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("""
+                ||||||||
+                Your turn!""");
     }
 }
